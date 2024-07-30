2825cc8e45168e391187f08013da7e133cba0c70
Lorenzo Nolasco <Lorenzonolasco23@gmail.com>
2024-07-29 19:15:50 -0600

    worked in file

--- server.js
+++ server.js
@@ -4,17 +4,21 @@ const express = require("express");
 // Import built-in Node.js package 'path' to resolve path of files that are located on the server
 const path = require("path");
 
+const apiRoutes = require("./routes/apiRoutes");
+const htmlRoutes = require("./routes/htmlRoutes");
+
 //initialize an instance of express.js
 const app = express();
 
 //PORT
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 //Static  middleware pointing to the public folder
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-app.get("/homepage", (req, res) =>
-  res.sendFile(path.join(__dirname, "public/index.html"))
-);
+app.use("/api/notes", apiRoutes);
+app.use("/", htmlRoutes);
 
-app.listen(PORT, () => console.log(`http://localhost:${PORT} Activated...`))
+app.listen(PORT, () => console.log(`http://localhost:${PORT} Activated...`));
